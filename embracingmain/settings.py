a75a63d2b204082fa959a47581cb77a371d33851
Jimin Msughter <yuwamsughter@gmail.com>
2025-10-28 12:19:20 +0100

    Initial commit: project import

--- embracingmain/settings.py
+++ embracingmain/settings.py
@@ -73,6 +73,7 @@ WSGI_APPLICATION = 'embracingmain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
